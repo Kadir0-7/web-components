52fbd59b12051308a610cc7c8da457108b0bb3db
Kadir0-7 <101948177+Kadir0-7@users.noreply.github.com>
2022-08-30 19:46:33 -0400

    viteConfig

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,26 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
+    },
+  },
   plugins: [
     VitePWA({
- registerType: 'autoUpdate',
+      registerType: 'autoUpdate',
       devOptions: {
         enabled: true,
-         
       },
       strategies: 'injectManifest',
-     injectManifest: {
-      filename: './sw.js',
-          globPatterns: ['**/*.html'],
-        },
-      
+      injectManifest: {
+        filename: './sw.js',
+        globPatterns: ['**/*.html'],
+      },
     }),
   ],
 });
